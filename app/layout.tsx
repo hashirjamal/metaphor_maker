a4453a018d183004877e3bc38d85568b2ff0c1b6
Hashir Jamal <hashirjamal18@gmail.com>
2025-06-24 01:34:29 +0500

    UI enhancements

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
     >
       <html lang="en" className='dark'>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <header className="flex justify-end items-center p-4 gap-4 h-16 bg-transparent">
             <SignedOut>
               <SignInButton />
               <SignUpButton />
--- components/composites/Hero.tsx
+++ components/composites/Hero.tsx
@@ -65,7 +65,7 @@ export function HeroSectionOne() {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+          <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium cursor-pointer text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             <Link href={"/metaphor"}>
 
               Explore Now
