1c4b85bc007cf62da265e89f9b1e6efd14b2d5d5
Hashir Jamal <hashirjamal18@gmail.com>
2025-06-26 03:48:22 +0500

    Mongoose bug fix

--- model.ts
+++ model.ts
@@ -1,14 +1,14 @@
 import mongoose, { Schema, Document, Model, models } from 'mongoose';
 
 const ContentSchema = new Schema<IContent>({
-  userId: { type: String, required: true },  
+  userId: { type: String, required: true },
   algoTitle: { type: String, required: true },
   algoSteps: { type: String, required: true },
   metaphorName: { type: String, required: true },
   metaphorDesc: { type: String, required: true },
   src: { type: String, required: true },
 }, {
-  timestamps: true 
+  timestamps: true
 });
 
 export const MetaphorModel: Model<IContent> = models.Metaphor || mongoose.model<IContent>('Metaphor', ContentSchema);
